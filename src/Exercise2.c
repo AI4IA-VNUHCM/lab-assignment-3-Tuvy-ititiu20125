fde4dd374891c198dd18c277c32a71c978f1c14b
Tuvy-ititiu20125 <ITITIU20125@student.hcmiu.edu.vn>
2021-10-18 17:36:26 +0700

    update ex

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -17,30 +17,28 @@ void Ex2(int arr[], int n)
 	int i;
 	for (i = 0; i < n; i++)
 	{
-		for (int i = 0; i < n - 1; i++)
-			for (int j = i + 1; j < n; j++)
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] % 2 == 0 && arr[j] % 2 == 0)
 			{
-				if (arr[i] % 2 == 0 && arr[j] % 2 == 0)
+				if (arr[i] < arr[j])
 				{
-					if (arr[i] < arr[j])
-					{
-						int temp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = temp;
-					}
-				}
-				if (arr[i] % 2 != 0 && arr[j] != 0)
-				{
-					if (arr[i] > arr[j])
-					{
-						int temp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = temp;
-					}
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
-
-		printf("%d ",arr[i]);
+			if (arr[i] % 2 != 0 && arr[j] % 2 != 0)
+			{
+				if (arr[i] > arr[j])
+				{
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		printf("%d ", arr[i]);
 	}
 }
 
--- src/Exercise5.c
+++ src/Exercise5.c
@@ -11,20 +11,42 @@ ______________________________________
 #include <stdlib.h>
 #include <math.h>
 
-void Ex5(int arr[], int n){
-	//Your codes here
-	
+void Ex5(int arr[], int n)
+{
+	int temp[n];
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] >= 0)
+		{
+			int temp[n];
+			temp[j++] = arr[i];
+		}
+		if (j == n || j == 0)
+			return;
+		for (int i = 0; i < n; i++)
+			;
+		{
+			if (arr[i] < 0)
+			{
+				temp[j++] = arr[i];
+			}
+		}
+		printf ("%d ",arr[i]);
+	}
 }
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
+int main(int argc, char *argv[])
+{
+	// testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		testcase[i] = atoi(argv[i+1]);
+	int testcase[argc], i;
+	for (i = 0; i < argc; i++)
+	{
+		testcase[i] = atoi(argv[i + 1]);
 	}
-	
+
 	Ex5(testcase, argc);
-	
+
 	return 0;
 }
